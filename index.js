7c5c8f6279bb347a3f0f5b1673ef9803d0076c96
elliottabirch <elliottabirch@gmail.com>
2017-05-01 16:26:12 -0700

    fix file names

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const calcDisagreement = require('./calcDisagreement');
 const calcAgreement = require('./calcAgreement');
 const calcGroupSizeScore = require('./calcGroupSizeScore');
-const calcIndividualScore = require('./calcIndividualScore');
-const generateMutatedIndividual = require('./generateMutatedIndividual');
+const calcArrangementScore = require('./calcArrangementScore');
+const generateMutatedArrangment = require('./generateMutatedArrangment');
 const generateOffspring = require('./generateOffspring');
-const generateRandomIndividual = require('./generateRandomIndividual');
+const generateRandomArrangment = require('./generateRandomArrangment');
 
 
 const dummy = require('./Genetic_Algo_Stuff');
