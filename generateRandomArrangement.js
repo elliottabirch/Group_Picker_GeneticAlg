acfbb71a27f76250728997263964cff86cec80d3
Dylan Gould <dylangood@gmail.com>
2017-05-01 16:52:05 -0700

    Create an arrangement of approximately the right number of groups
    Although it is empty.

--- generateRandomArrangement.js
+++ generateRandomArrangement.js
@@ -1,3 +1,24 @@
-module.exports = function () {
+module.exports = function (individuals) {
+  var numIndividuals = individuals.length;
+  // starting with the number of individuals, find about how many groups we want to form
+  var numGroups = Math.ceil(Math.random() * numIndividuals);
+  // start an arrangement
+  var arrangement = [];
+  for ( var i = 0; i < numGroups; i++ ) {
+    arrangement.push( { 
+      members: [],
+      score: 0
+    });
+  }
 
+  // forEach individual in individuals
+    // get the individual's name
+    // generate a random number from 0 to numGroups
+    // add the name to the randomth element of arrangement
+    // 
+
+
+  // calculate the group score of each group in arrangement
+  
+  return arrangement;
 };
