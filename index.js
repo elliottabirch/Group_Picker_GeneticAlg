6619953476a5ee49ddee30cf7e672b1f17904b9f
elliottabirch <elliottabirch@gmail.com>
2017-05-01 16:09:07 -0700

    require helper functions

---
+++ index.js
@@ -0,0 +1,8 @@
+var calcDisagreement require('./calcDisagreement')
+var calcAgreement require('./calcAgreement')
+var calcGroupSizeScore require('./calcGroupSizeScore')
+var calcIndividualScore require('./calcIndividualScore')
+var generateMutatedIndividual require('./generateMutatedIndividual')
+var generateOffspring require('./generateOffspring')
+var generateRandomIndividual require('./generateRandomIndividual')
+
