626e26e23d1d77aca4418f948b6c68206a5d9c7a
elliottabirch <elliottabirch@gmail.com>
2017-05-01 17:35:55 -0700

    implement mutation generator

--- generateMutatedArrangement.js
+++ generateMutatedArrangement.js
@@ -1,3 +1,11 @@
-module.exports = function () {
-
+module.exports = function (arrangement) {
+  const percentMutation = 0.10;
+  const numberOfGroups = Math.ceil(arrangement.length / 4);
+  return arrangement.map((number) => {
+    if (Math.random() <= percentMutation) {
+      return Math.round(numberOfGroups * Math.random());
+    }
+    return number;
+  });
 };
+
