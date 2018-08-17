0159ab6ab9f0e9d52c2feea57e3d2abfacc65bc3
elliottabirch <elliottabirch@gmail.com>
2018-08-17 14:29:00 -0700

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Group_Picker_GeneticAlg
+
+Create the most optimal permutation of groups for group projects in a classroom setting. These groups can have no more than 5 people in them
+
+Each participant has defined the people they want to work with and people they don't want to work with.
+
+How algorithm works
+
+1. Random groups are created
+2. Groups are scored:
+	a. Being in a group with an enemy gives -5 points to group score.
+	b. Being in a group with a friend gives +1 point to group score.
+	c. Having a group larger than 5 people gives -1000 points to a group score.
+3. Groups are sorted by score.
+4. the worst group permutations are removed from population
+5. New permutations are created from remaining permutaions
+6. 2-5 are repeated until a goal is achieved, and printed out in solutions.txt
+
+TO RUN:
+-
+Group data is in Genetic_Algo_Stuff.json
+
+run --  node index.js --  in terminal
+
+Find 4 optimal group permutations in solution.txt
--- index.js
+++ index.js
@@ -10,55 +10,55 @@ const fs = require('fs');
 
 const dummy = require('./organizeDummyData');
 
+const write = function (gen) {
+  fs.appendFile('solution.txt', (function (gen) {
+    let results = `${gen[0].score}\n`;
+    const groups = gen[0].arrangement.map(group => group.members.map(index => [dummy[index].name]));
+    groups.forEach((group, index) => {
+      results += `${index}. ${group}\n`;
+    });
+    return results;
+  }(gen)));
+};
+
+const log = function (gen) {
+  console.log(gen[0].arrangement.map((group) => {
+    const members = group.members.map(index => [dummy[index].name, calcDisagreement(dummy, dummy[index].disagreements, group.members), calcAgreement(dummy, dummy[index].agreements, group.members)]);
+    return ({
+      agreements: members.reduce((accum, group) => accum + group[2], 0),
+      disagreements: members.reduce((accum, group) => accum + group[1], 0),
+    });
+  }), gen[0].score);
+  console.log(gen[1].arrangement.map((group) => {
+    const members = group.members.map(index => [dummy[index].name, calcDisagreement(dummy, dummy[index].disagreements, group.members), calcAgreement(dummy, dummy[index].agreements, group.members)]);
+    return ({
+      agreements: members.reduce((accum, group) => accum + group[2], 0),
+      disagreements: members.reduce((accum, group) => accum + group[1], 0),
+    });
+  }), gen[1].score);
+};
+
 const calculate = function () {
   let generation = [];
   const populationSize = 25;
 
 
-// generate random population
-  for (var i = 0; i < populationSize; i++) {
+  // generate random population
+  for (let i = 0; i < populationSize; i++) {
     generation[i] = generateRandomGenome(dummy);
   }
 
   generation = generatePopulation(generation, dummy, 4);
 
-  for (var gen = 0; gen < 3000; gen++) {
+  for (let gen = 0; gen < 5000; gen++) {
   // console.log(nextGen[0].score, nextGen[0].score);
     generation = generateNextGeneration(generation, dummy, 4, 0.2);
   }
 
-  const log = function (gen) {
-    console.log(gen[0].arrangement.map((group) => {
-      const members = group.members.map(index => [dummy[index].name, calcDisagreement(dummy, dummy[index].disagreements, group.members), calcAgreement(dummy, dummy[index].agreements, group.members)]);
-      return ({
-        agreements: members.reduce((accum, group) => accum + group[2], 0),
-        disagreements: members.reduce((accum, group) => accum + group[1], 0),
-      });
-    }), gen[0].score);
-    console.log(gen[1].arrangement.map((group) => {
-      const members = group.members.map(index => [dummy[index].name, calcDisagreement(dummy, dummy[index].disagreements, group.members), calcAgreement(dummy, dummy[index].agreements, group.members)]);
-      return ({
-        agreements: members.reduce((accum, group) => accum + group[2], 0),
-        disagreements: members.reduce((accum, group) => accum + group[1], 0),
-      });
-    }), gen[1].score);
-  };
-
-
-  const write = function (gen) {
-    fs.appendFile('solution.txt', (function (gen) {
-      let results = `${gen[0].score}\n`;
-      const groups = gen[0].arrangement.map(group => group.members.map(index => [dummy[index].name]));
-      groups.forEach((group, index) => {
-        results += `${index}. ${group}\n`;
-      });
-      return results;
-    }(gen)));
-  };
 
   write(generation);
 };
 
-for (var i = 0; i < 4; i++) {
+for (let i = 0; i < 4; i++) {
   calculate();
 }
